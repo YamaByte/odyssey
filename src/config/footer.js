f2c044ba02370c55497c6804493cf29dd5af2a1e
StatisticianOne <markchangg@outlook.com>
2025-10-24 06:51:47 +0000

    replaced some assets with YamaByte assets, and removed some templates

---
+++ src/config/footer.js
@@ -0,0 +1,48 @@
+export const footerSocials = [
+  {
+    name: 'LinkedIn',
+    url: 'https://linkedin.com/company/yamabyte',
+    icon: "mdi:linkedin",
+  },
+]
+
+export const footerLists = [
+	{
+		title: 'Company',
+		items: [
+			{
+				title: 'About',
+				slug: '/about',
+			},
+			{
+				title: 'Blog',
+				slug: '/blog',
+			},
+			{
+				title: 'Contact',
+				slug: '/contact',
+			},
+		],
+	},
+	{
+		title: 'Theme',
+		items: [
+			{
+				title: 'Get Started',
+				slug: '/theme/get-started',
+			},
+			{
+				title: 'Style Guide',
+				slug: '/theme/style-guide',
+			},
+      {
+        title: 'Theme Setup',
+        slug: '/theme/theme-setup',
+      },
+      {
+        title: 'Customizing Odyssey',
+        slug: '/theme/customizing-odyssey',
+      }
+		],
+	},
+];
---
+++ src/config/settings.js
@@ -0,0 +1,8 @@
+export default {
+	title: `YamaByte | Your Boutique AI/ML Consulting Partner`,
+	description: `Boutique consultancy founded by PhD AI researcher and professional Data Scientist who had hands-on development and strategic experience across many industries`,
+	url: `https://odyssey.yamabyte.com`, // No trailing slash!
+	name: `YamaByte`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	enableThemeSwitcher: false,
+	showPlug: true, // Disable this if you want to remove the plug from the footer. (╯°□°)╯︵ ┻━┻
+};
